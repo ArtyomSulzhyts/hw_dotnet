9738820343f1d3eee86122c06b4ff116f9d9150b
asulzhuts <Let5Java>
2020-11-10 22:11:22 +0300

    HW04 Task 6 - Alphabet

---
+++ HW04.Task6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW04.Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstLetter = 97;
+            int lastLetter = 122;
+            int alphabetLength = lastLetter - firstLetter + 1;
+
+            char[] alphabet = new char[alphabetLength];
+
+            for (int i = 0; i < alphabetLength; i++)
+            {
+                char letter = Convert.ToChar(lastLetter);
+                alphabet[i] = letter;
+                lastLetter--;
+            }
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                Console.Write(alphabet[i]);
+            }
+        }
+    }
+}
