f09ab2e8ca16f7dfd23d5e69ae3f52f56eeea4be
asulzhuts <Let5Java>
2020-11-10 22:12:59 +0300

    HW04 Task7 - Moving Figure

---
+++ HW04.Task7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW04.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите W, S, A или D");
+            string move = Console.ReadLine();
+            switch (move)
+            {
+                case "W":
+                    Console.WriteLine("Фигура перемещается ВВЕРХ");
+                    break;
+                case "S":
+                    Console.WriteLine("Фигура перемещается ВНИЗ");
+                    break;
+                case "A":
+                    Console.WriteLine("Фигура перемещается ВЛЕВО");
+                    break;
+                case "D":
+                    Console.WriteLine("Фигура перемещается ВПРАВО");
+                    break;
+                default:
+                    Console.WriteLine("Вы нажали не W, S, A или D");
+                    break;
+            }
+        }
+    }
+}
